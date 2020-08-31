b8daffefe7dadb3e870ca68f4962b0a1edaa23bd
Jonathan Getahun <kgetahun2@gmail.com>
2020-08-31 15:24:56 -0400

    part 2 finish

--- part_2/the_phonebook/src/App.js
+++ part_2/the_phonebook/src/App.js
@@ -1,103 +1,125 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import Notification from './components/Notification'
+import Filter from './components/Filter'
+import personService from './services/persons'
 
-const Add = ({name,number}) => {
-  return <h5>{name.name} {number.number}</h5>
-}
-
-// const AddPerson = ({person}) => {
-//   console.log("hello",person)
-//   useEffect(() => {
-//     axios.post('http://localhost:3001/persons',person) 
-//     .then(response => {
-//     console.log(response)
-//   })
-// }, [])}
 const App = () => {
-  
-  const [ persons, setPersons ] = useState([]) 
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-  const [ filterz, setNewFilter ] = useState('')
+  const [ filterString, setStringFilter ] = useState('')
+  const [ notification, setNotification ] = useState(null)
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-    .then(response => {
-      console.log(response.data)
-    })
+    personService
+      .getAll()
+      .then((data) => {
+        setPersons(data)
+      })
+
   }, [])
 
-  
+  const notifyWith = (message, type='success') => {
+    setNotification({ message, type })
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  }
 
-
-  const handleChangeName = (event) =>{
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
-  }
-
-  const handleChangeNumber = (event) =>{
+  }  
+  
+  const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
-  }
+  }  
 
-  const handleChangeFilter = (event) => {
-    setNewFilter(event.target.value)
-  }
+  const handleFilterStringChange = (event) => {
+    setStringFilter(event.target.value)
+  } 
 
-  const handleClick = (event) =>{
-    event.preventDefault();
-    
-    if (persons.find(element => element.name === newName)) {
-      window.alert(`${newName} has already been added`)
-    } else {
-      const addName = {name: newName, number: newNumber}
-      axios.post('http://localhost:3001/persons',addName) 
-      .then(response => {
-      console.log(response)})
-      setPersons(persons.concat(addName))
-      // return <AddPerson person = {persons}/>
+  const deletePerson = (id) => {
+    const toDelete = persons.find(p => p.id === id)
+    const ok = window.confirm(`Delete ${toDelete.name}`)
+    if (ok) {
+      personService.remove(id)
+        .then(response => {
+          setPersons(persons.filter(p => p.id !== id))
+          notifyWith(`Deleted ${toDelete.name}`)
+        }).catch(() => {
+          setPersons(persons.filter(p => p.id !== id))
+          notifyWith(`${toDelete.name} had already been removed`, 'error')
+        })
     }
-    
-    setNewName('')
-    setNewNumber('')
   }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existing = persons.find(p => p.name === newName)
+    if (existing) {
+      const ok = window.confirm(`${existing.name} already in phonebook, replace the old number with new one?`)
+      if (ok) {
+        personService.update(existing.id, {
+          name: existing.name,
+          number:newNumber
+        }).then(retunedPerson => {
+          setPersons(persons.map(person => person.id !== existing.id ? person : retunedPerson))
+          notifyWith(`Changed number of  ${existing.name}`)
+          setNewName('')
+          setNewNumber('')
+        })
+      }
+
+    } else {
+      personService.create({
+        name: newName,
+        number: newNumber   
+      }).then(addedPerson => {
+        setPersons(persons.concat(addedPerson))
+        notifyWith(`Added ${newName}`)
+        setNewName('')
+        setNewNumber('')
+      }).catch(error => {
+        // pääset käsiksi palvelimen palauttamaan virheilmoitusolioon näin
+        console.log(error.response.data.error)
+        notifyWith(`${error.response.data.error} `, 'error')
+      })
+    }
+  }
+
+  const personsToShow = filterString.length === 0 ?
+    persons : 
+    persons.filter(p => p.name.toLowerCase().indexOf(filterString.toLowerCase()) > 0 )
 
   return (
-    
     <div>
-      <form>
-        <div>
-          filter shown with <input value={filterz} onChange={handleChangeFilter}/>
-        </div>
-      </form>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input value={newName} onChange={handleChangeName}/>
-        </div>
-        <div>
-        number: <input value={newNumber} onChange={handleChangeNumber} />
-        </div>
-        <div>
-          <button type="submit" onClick={handleClick}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      
-       {
-          persons.map(person => {
-            if(filterz === ""){
-               return <Add name={person} number={person} key={person.id} />
-            } else {
-              console.log(filterz.toLowerCase())
-              if(person.name.toLowerCase().includes(filterz.toLowerCase())){
-                return <Add name={person} number={person} key={person.id} />
-              }
-            }
-          })
-            
-        } 
-  
+
+      <Notification notification={notification} />
+
+      filter shown with: 
+      <Filter
+        value={filterString}
+        onChange={handleFilterStringChange}
+      />
+
+      <h3>add a new</h3>
+      <PersonForm 
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        newNumber={newNumber}
+        newName={newName}
+        addPerson={addPerson}
+      />
+     
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} deletePerson={deletePerson}/>
     </div>
   )
+
 }
 
-export default App
+export default App 
